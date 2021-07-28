68ac5fe8c89a7bfd6dfc26d73a4834c881a34bcf
bluaura@gmail.com <bluaura@gmail.com>
2021-07-28 21:32:37 +0900

    [study][python][basics] built-in methods
    [desc]
    built-in methods

---
+++ builin_methods.py
@@ -0,0 +1,23 @@
+'''
+
+    built in methods
+
+'''
+
+
+# join
+print('1. join() method')
+list = ['h', 'e', 'l', 'l', 'o']
+print(' print list')
+print(list)
+print(' join()ed list')
+list2 = ''.join(list)
+print(list2)
+print()
+
+print('2. replace(before, after)')
+text = 'book'
+print(text)
+text2 = text.replace('b', 'c')
+print(text2)
+print()
---
+++ python_user_modules.py
@@ -0,0 +1,13 @@
+'''
+user defined module import
+
+from [directory name] import [python file]
+'''
+
+from Module1.mod1 import test_module1
+from Module1.mod2 import test_module2
+
+test_module1.mod1_test1()
+test_module1.mod1_test2()
+test_module2.mod2_test1()
+test_module2.mod2_test2()
